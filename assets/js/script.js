fb744fb050155415c1d410992c58d1efcdb3c4e0
Jpost <Jpost2010@Gmail.com>
2023-08-02 13:21:58 -0400

    updated the HTML to create IDs for each unique button/text window. Updated JS to save and reload each local storage in the correct window box. Linked day.js in order to display both the current day as well as to take the current time in order to change the color of each box depending on the hour of the day by changing their classes through hs to 'past', 'present', or 'future' accordingly.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,129 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
+
+  var today = dayjs();
+  var currentTime = dayjs().format('H');
+  $('#currentDay').text(today.format("[Today's date is: ] MMMM D, YYYY"));
+
+  //Sets each box to the localStorage created for each hour
+  $('#toDo9').val(localStorage.toDo9);
+  $('#toDo10').val(localStorage.toDo10);
+  $('#toDo11').val(localStorage.toDo11);
+  $('#toDo12').val(localStorage.toDo12);
+  $('#toDo13').val(localStorage.toDo13);
+  $('#toDo14').val(localStorage.toDo14);
+  $('#toDo15').val(localStorage.toDo15);
+  $('#toDo16').val(localStorage.toDo16);
+  $('#toDo17').val(localStorage.toDo17);
+
+    
+  //saves the inputted values to the correct localStorate object based on the button clicked to save it
+  $('#btn9').on('click', function(){
+    localStorage.toDo9 = $('#toDo9').val();
   });
+
+  $('#btn10').on('click', function(){
+    localStorage.toDo10 = $('#toDo10').val();
+  });
+
+  $('#btn11').on('click', function(){
+    localStorage.toDo11 = $('#toDo11').val();
+  });
+
+  $('#btn12').on('click', function(){
+    localStorage.toDo12 = $('#toDo12').val();
+  });
+
+  $('#btn13').on('click', function(){
+    localStorage.toDo13 = $('#toDo13').val();
+  });
+
+  $('#btn14').on('click', function(){
+    localStorage.toDo14 = $('#toDo14').val();
+  });
+
+  $('#btn15').on('click', function(){
+    localStorage.toDo15 = $('#toDo15').val();
+  });
+
+  $('#btn16').on('click', function(){
+    localStorage.toDo16 = $('#toDo16').val();
+  });
+
+  $('#btn17').on('click', function(){
+    localStorage.toDo17 = $('#toDo17').val();
+  });
+
+
+//added if-else statements to compare the the day.js time of day hour we get and set the class accordingly to match the color.
+  if(currentTime > 9){
+    $('#hour-9').addClass('past');
+  }else if(currentTime == 9){
+    $('#hour-9').addClass('present');
+  }else{
+    $('#hour-9').addClass('future');
+  }
+
+  if(currentTime > 10){
+    $('#hour-10').addClass('past');
+  }else if(currentTime == 10){
+    $('#hour-10').addClass('present');
+  }else{
+    $('#hour-10').addClass('future');
+  }
+
+  if(currentTime > 11){
+    $('#hour-11').addClass('past');
+  }else if(currentTime == 11){
+    $('#hour-11').addClass('present');
+  }else{
+    $('#hour-11').addClass('future');
+  }
+
+  if(currentTime > 12){
+    $('#hour-12').addClass('past');
+  }else if(currentTime == 12){
+    $('#hour-12').addClass('present');
+  }else{
+    $('#hour-12').addClass('future');
+  }
+
+  if(currentTime > 13){
+    $('#hour-13').addClass('past');
+  }else if(currentTime == 13){
+    $('#hour-13').addClass('present');
+  }else{
+    $('#hour-13').addClass('future');
+  }
+
+  if(currentTime > 14){
+    $('#hour-14').addClass('past');
+  }else if(currentTime == 14){
+    $('#hour-14').addClass('present');
+  }else{
+    $('#hour-14').addClass('future');
+  }
+
+  if(currentTime > 15){
+    $('#hour-15').addClass('past');
+  }else if(currentTime == 15){
+    $('#hour-15').addClass('present');
+  }else{
+    $('#hour-15').addClass('future');
+  }
+
+  if(currentTime > 16){
+    $('#hour-16').addClass('past');
+  }else if(currentTime == 16){
+    $('#hour-16').addClass('present');
+  }else{
+    $('#hour-16').addClass('future');
+  }
+
+  if(currentTime > 17){
+    $('#hour-17').addClass('past');
+  }else if(currentTime == 17){
+    $('#hour-17').addClass('present');
+  }else{
+    $('#hour-17').addClass('future');
+  }
+});
